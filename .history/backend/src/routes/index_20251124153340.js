46db9f6b64c25328f041583cd35c45c803d23c3e
Saar45 <nabysarr16@gmail.com>
2025-11-25 05:01:03 +0100

    untrack .history

--- .history/backend/src/routes/index_20251124153340.js
+++
@@ -1,9 +0,0 @@
-const itemRoutes = require('./itemRoutes');
-const questRoutes = require('./questRoutes');
-const playerRoutes = require('./playerRoutes');
-
-module.exports = {
-  itemRoutes,
-  questRoutes,
-  playerRoutes
-};
